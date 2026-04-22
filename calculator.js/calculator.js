7aa9354476b554d8996d62c3f004a2ed31ab2b60
gcondinato-coder <gcondinato@spamast.edu.ph>
2026-04-22 18:20:56 +0800

    Step 10

--- calculator.js/calculator.js
+++ calculator.js/calculator.js
@@ -1,6 +1,3 @@
-function calculateSum(num1, num2) {
-  return num1 + num2;
+function calculateDifference(num1, num2) {
+  return num1 - num2;
 }
-console.log(calculateSum(2, 5));
-console.log(calculateSum(10, 10));
-console.log(calculateSum(5, 5));
