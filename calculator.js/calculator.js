f03d9fa826f70f12ee1ea8cf19887c8ea64e1b50
gcondinato-coder <gcondinato@spamast.edu.ph>
2026-04-22 18:22:36 +0800

    Step 13

--- calculator.js/calculator.js
+++ calculator.js/calculator.js
@@ -1,4 +1,4 @@
-function calculateProduct(num1, num2) {
-  return num1 * num2;
+function calculateQuotient(num1, num2) {
+  return num1 / num2;
 }
-console.log(calculateProduct(13, 5));
+console.log(calculateQuotient(7, 11));
